09f16334774dafe88106d7fbfa5e3a47ca94e7b2
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2021-02-22 23:19:45 +0000

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Camin McCluskey on 22/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
